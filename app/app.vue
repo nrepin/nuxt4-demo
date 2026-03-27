73420f4564307d9b7c0d02998c0fabad92937807
nrepin <web.repin@yandex.ru>
2026-03-27 11:28:46 +0300

    Initial

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@use "~/assets/scss/main.scss";
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    },
+  },
+})
